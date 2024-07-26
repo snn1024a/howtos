23610a7b2ddf30c846e95c01a42770943307ade0
Stoyan Shterev <snn1024a@users.noreply.github.com>
2024-07-26 18:06:38 +0100

    Update gpio-led.cmd

--- raspberry-pi/gpio-led.cmd
+++ raspberry-pi/gpio-led.cmd
@@ -1,7 +1,6 @@
-https://photos.google.com/photo/AF1QipNM_r1HGxHS9UrmBXIfl69Nk_jVtE4T0AKaIngR
-
-
 gpiofind GPIO24
 gpioget gpiochip0 24
 gpioset gpiochip0 24=1
 while :;do gpioset gpiochip0 24=0;sleep 1;gpioset gpiochip0 24=1;sleep 1;done
+
+https://photos.google.com/photo/AF1QipNM_r1HGxHS9UrmBXIfl69Nk_jVtE4T0AKaIngR
